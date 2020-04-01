b99fcf3ec1ea85c8f75cf5d4b53c90a90e5939f5
Phil Adams <phil_adams@us.ibm.com>
2020-04-01 15:14:41 -0500

    fix(build): add npm deployment to build

--- scripts/typedoc/generate_typedoc.sh
+++ scripts/typedoc/generate_typedoc.sh
@@ -1,8 +1,8 @@
 
-# For each service belonging to your project, add a command like the commented out example below.
-# Replace "service-name/v1.ts" with the source directory name and version for the service.
-#./node_modules/.bin/typedoc --mode file --theme ./scripts/typedoc/theme --excludeExternals \
-#    --out ./service-name/v1.ts --target "ES5"
-
-# List the commands for each service here:
-    
+# For each service belonging to your project, add a reference to its
+# generated source file to the command below.
+./node_modules/.bin/typedoc --mode file --theme ./scripts/typedoc/theme --excludeExternals --excludePrivate --excludeProtected \
+    --out ./doc \
+    ./global-search/v2.ts \
+    ./global-tagging/v1.ts \
+    --target "ES5"

3ac4d83dd6b13ae70a8edae831179355a1eb5b4a
IBM Cloud SDK Developement <devxsdk@us.ibm.com>
2020-04-05 13:40:04 -0500

    test: fix up integration tests and add config file

--- modules/global-search/src/test/java/com/ibm/cloud/platform_services/global_search/v2/GlobalSearchIT.java
+++ modules/global-search/src/test/java/com/ibm/cloud/platform_services/global_search/v2/GlobalSearchIT.java
@@ -13,105 +13,125 @@
 
 package com.ibm.cloud.platform_services.global_search.v2;
 
-import com.ibm.cloud.sdk.core.http.Response;
-import com.ibm.cloud.sdk.core.security.IamAuthenticator;
-import com.ibm.cloud.platform_services.global_search.v2.model.ScanResult;
-import com.ibm.cloud.platform_services.global_search.v2.model.SearchOptions;
-import com.ibm.cloud.platform_services.global_search.v2.model.SearchOptions.Builder;
-import com.ibm.cloud.platform_services.global_search.v2.model.SupportedTypesList;
-import com.ibm.cloud.sdk.core.service.exception.NotFoundException;
-import com.ibm.cloud.sdk.core.service.exception.RequestTooLargeException;
-import com.ibm.cloud.sdk.core.service.exception.ServiceResponseException;
-
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.fail;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+
+import com.ibm.cloud.platform_services.global_search.v2.model.ScanResult;
+import com.ibm.cloud.platform_services.global_search.v2.model.SearchOptions;
+import com.ibm.cloud.platform_services.global_search.v2.model.SupportedTypesList;
+import com.ibm.cloud.platform_services.test.SdkIntegrationTestBase;
+import com.ibm.cloud.sdk.core.http.Response;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.cloud.sdk.core.service.exception.ServiceResponseException;
 
 /**
  * Integration test class for the GlobalSearch service.
  */
-public class GlobalSearchIT {
-	
-	Properties prop = null;
-	GlobalSearch globalSearch = null;
-	
-    @Test
-    public void testSearch() throws Exception, InterruptedException {
-        System.out.println("START GlobalSearchIT.testSearch method");
-        
+public class GlobalSearchIT extends SdkIntegrationTestBase {
+
+    Properties prop = null;
+    GlobalSearch globalSearch = null;
+
+    /**
+     * This method provides our config filename to the base class.
+     */
+    public String getConfigFilename() {
+        return "../../ghost.env";
+    }
+
+    /**
+     * This method is invoked before any @Test-annotated methods, and is responsible
+     * for creating the instance of the service that will be used by the rest of the
+     * test methods, as well as any other required initialization.
+     */
+    @BeforeClass
+    public void constructService() {
+        // Ask super if we should skip the tests.
+        if (skipTests()) {
+            return;
+        }
+
         try {
-            // Test 1 - search all resource
-            Builder builder = new SearchOptions.Builder().query("name:gst-sdk*");
-            SearchOptions searchOpt = builder.build();
+            InputStream input = new FileInputStream(getConfigFilename());
+            this.prop = new Properties();
+            this.prop.load(input);
+        } catch (Throwable t) {
+            System.out.println(
+                    String.format("Unable to load properties file %s: %s", getConfigFilename(), t.getMessage()));
+            this.skipTests = true;
+            return;
+        }
+
+        // Create the IAM authenticator.
+        IamAuthenticator authenticator = new IamAuthenticator(this.prop.getProperty("GST_IINTERNA_APIKEY"));
+        authenticator.setURL(this.prop.getProperty("GST_IAM_URL"));
+
+        // Create the service instance.
+        this.globalSearch = new GlobalSearch("global_search", authenticator);
+        this.globalSearch.setServiceUrl(this.prop.getProperty("GST_API_URL"));
+    }
+
+    @Test
+    public void testSearchAll() {
+        System.out.println("START GlobalSearchIT.testSearchAll method");
+
+        try {
+            // Test - search all resource
+            SearchOptions searchOpt = new SearchOptions.Builder()
+                    .query("name:gst-sdk*")
+                    .build();
             Response<ScanResult> response = this.globalSearch.search(searchOpt).execute();
             assertNotNull(response);
             ScanResult result = response.getResult();
-            System.out.println("response search: " + result.getItems());
             assertNotNull(result);
+            System.out.println("response: " + result.getItems());
             assertEquals(result.getItems().size(), 2);
- 
-            // Test 2 - search all resource and set limit = 1
-            Builder builder2 = new SearchOptions.Builder()
-                .query(this.prop.getProperty("GST_QUERY"))
-                .limit(1)
-                .addFields("crn")
-                .addFields("name");
-            SearchOptions searchOpt2 = builder2.build();
-            Response<ScanResult> response2 = this.globalSearch.search(searchOpt2).execute();
-            assertNotNull(response2);
-            ScanResult result2 = response2.getResult();
-            assertNotNull(result2);
-            assertEquals(result2.getItems().size(), 1);
-            System.out.println("response search 2: " + result2.getItems());
-        } catch (NotFoundException e) {
-            // Handle Not Found (404) exception
-            System.out.println("Service returned status code " + e.getStatusCode() + ": " + e.getMessage());
-        } catch (RequestTooLargeException e) {
-            // Handle Request Too Large (413) exception
-            System.out.println("Service returned status code " + e.getStatusCode() + ": " + e.getMessage());
         } catch (ServiceResponseException e) {
-            // Base class for all exceptions caused by error responses from the service
-            System.out.println("Service returned status code " + e.getStatusCode() + ": " + e.getMessage());
-            System.out.println("Error details: " + e.getDebuggingInfo());
+            fail(String.format("Service returned status code %s: %s\nError details: %s",
+                    e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()));
         }
-     }
-    
-    @Test
-    public void testGetSupportedTypes() throws Exception, InterruptedException {
-   	   // Test 3 - getSupportedTypes operation
-       Response<SupportedTypesList> responseSTL = this.globalSearch.getSupportedTypes().execute();
-       assertNotNull(responseSTL);
-       SupportedTypesList resultSTL = responseSTL.getResult();
-       assertNotNull(resultSTL);
-       System.out.println("response SupportedTypesList: " + resultSTL.getSupportedTypes());
-       
-   	
-   }
-    
-    @BeforeSuite
-    public void setUp() throws Exception, InterruptedException {
-    	System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        //load file proerties
+    }
+
+    @Test(dependsOnMethods = { "testSearchAll" })
+    public void testSearchOne() {
+        System.out.println("START GlobalSearchIT.testSearchOne method");
+
         try {
-          InputStream input = new FileInputStream(System.getProperty("user.dir") + "/../../.ghostenv");
-          this.prop = new Properties();
-          this.prop.load(input);
-        } catch (IOException ex) {
-          ex.printStackTrace();
+            // Test - search all resource and set limit = 1
+            SearchOptions searchOpt = new SearchOptions.Builder()
+                    .query(this.prop.getProperty("GST_QUERY"))
+                    .limit(1)
+                    .addFields("crn")
+                    .addFields("name")
+                    .build();
+            Response<ScanResult> response = this.globalSearch.search(searchOpt).execute();
+            assertNotNull(response);
+
+            ScanResult result = response.getResult();
+            assertNotNull(result);
+            assertEquals(result.getItems().size(), 1);
+            System.out.println("response: " + result.getItems());
+        } catch (ServiceResponseException e) {
+            fail(String.format("Service returned status code %s: %s\nError details: %s", e.getStatusCode(),
+                    e.getMessage(), e.getDebuggingInfo()));
         }
-        //Create the IAM authenticator.
-        IamAuthenticator authenticator = new IamAuthenticator(this.prop.getProperty("GST_IINTERNA_APIKEY"));
-        authenticator.setURL(this.prop.getProperty("GST_IAM_URL"));
-        this.globalSearch = new GlobalSearch("global_search", authenticator);
-        this.globalSearch.setEndPoint(this.prop.getProperty("GST_API_URL"));
+    }
+
+    @Test(dependsOnMethods = { "testSearchOne" })
+    public void testGetSupportedTypes() {
+        // Test 3 - getSupportedTypes operation
+        Response<SupportedTypesList> responseSTL = this.globalSearch.getSupportedTypes().execute();
+        assertNotNull(responseSTL);
+        SupportedTypesList resultSTL = responseSTL.getResult();
+        assertNotNull(resultSTL);
+        System.out.println("response SupportedTypesList: " + resultSTL.getSupportedTypes());
     }
 }

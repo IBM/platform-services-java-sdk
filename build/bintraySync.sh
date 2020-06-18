8a199b05bbc8b907e0fb6d873d698e73f8db5f8b
Phil Adams <phil_adams@us.ibm.com>
2020-06-18 12:43:52 -0500

    chore: add ipm package name to bintraySync script [skip ci]

--- build/bintraySync.sh
+++ build/bintraySync.sh
@@ -27,6 +27,7 @@ com.ibm.cloud:global-catalog
 com.ibm.cloud:global-search
 com.ibm.cloud:global-tagging
 com.ibm.cloud:iam-access-groups
+com.ibm.cloud:iam-policy-management
 com.ibm.cloud:resource-manager"
 
 this_dir=`dirname "$0"`

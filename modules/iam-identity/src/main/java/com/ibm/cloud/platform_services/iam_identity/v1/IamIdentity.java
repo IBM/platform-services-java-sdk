24c98b3796bfeb8c4d058f6553671364b802dfdc
Phil Adams <phil_adams@us.ibm.com>
2020-09-28 16:50:21 -0500

    chore: re-gen with production API definition

--- modules/iam-identity/src/main/java/com/ibm/cloud/platform_services/iam_identity/v1/IamIdentity.java
+++ modules/iam-identity/src/main/java/com/ibm/cloud/platform_services/iam_identity/v1/IamIdentity.java
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-b734cb71-20200916-090210
+ * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-6748b103-20200928-080640
  */
 
 package com.ibm.cloud.platform_services.iam_identity.v1;
@@ -58,7 +58,7 @@ public class IamIdentity extends BaseService {
 
   public static final String DEFAULT_SERVICE_NAME = "iam_identity";
 
-  public static final String DEFAULT_SERVICE_URL = "https://iam.test.cloud.ibm.com";
+  public static final String DEFAULT_SERVICE_URL = "https://iam.cloud.ibm.com";
 
  /**
    * Class method which constructs an instance of the `IamIdentity` client.

16a46f9c1850bf14f7a250971d8b0c3242da060e
Phil Adams <phil_adams@us.ibm.com>
2020-11-23 09:39:20 -0600

    fix(build): revert to using gnupg 1.x

--- build/setupSigning.sh
+++ build/setupSigning.sh
@@ -3,7 +3,7 @@ set -x
 
 echo "Importing signing key..."
 openssl aes-256-cbc -K $encrypted_4c2ca4326cdc_key -iv $encrypted_4c2ca4326cdc_iv -in build/signing.key.enc -out build/signing.key -d
-gpg2 --version
-gpg2 --fast-import build/signing.key
+gpg --version
+gpg --fast-import build/signing.key
 rm build/signing.key
 echo "Signing key import finished!"

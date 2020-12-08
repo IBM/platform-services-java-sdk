a8deb788d22fa20613bfc714385223ba5e11e5a9
Phil Adams <phil_adams@us.ibm.com>
2020-12-08 10:18:24 -0600

    fix(Configuration Governance): re-gen service with latest API

--- modules/configuration-governance/src/main/java/com/ibm/cloud/platform_services/configuration_governance/v1/ConfigurationGovernance.java
+++ modules/configuration-governance/src/main/java/com/ibm/cloud/platform_services/configuration_governance/v1/ConfigurationGovernance.java
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-af92e433-20201110-100619
+ * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-629bbb97-20201207-171303
  */
 
 package com.ibm.cloud.platform_services.configuration_governance.v1;

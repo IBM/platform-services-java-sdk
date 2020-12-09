0d0b1b40c675b842f7badae3cb86879277bb0674
Phil Adams <phil_adams@us.ibm.com>
2020-12-09 11:45:28 -0600

    fix(Global Tagging): re-gen service with latest API

--- modules/global-tagging/src/main/java/com/ibm/cloud/platform_services/global_tagging/v1/GlobalTagging.java
+++ modules/global-tagging/src/main/java/com/ibm/cloud/platform_services/global_tagging/v1/GlobalTagging.java
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-9b0d887a-20200923-132457
+ * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-629bbb97-20201207-171303
  */
 
 package com.ibm.cloud.platform_services.global_tagging.v1;

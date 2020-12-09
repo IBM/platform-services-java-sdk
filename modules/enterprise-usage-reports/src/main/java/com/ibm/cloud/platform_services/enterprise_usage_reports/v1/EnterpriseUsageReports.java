cd4cd2564054c47bfbc603f6d57e88c97b639563
Phil Adams <phil_adams@us.ibm.com>
2020-12-09 10:15:19 -0600

    fix(Enterprise Usage Reports): re-gen service with latest API

--- modules/enterprise-usage-reports/src/main/java/com/ibm/cloud/platform_services/enterprise_usage_reports/v1/EnterpriseUsageReports.java
+++ modules/enterprise-usage-reports/src/main/java/com/ibm/cloud/platform_services/enterprise_usage_reports/v1/EnterpriseUsageReports.java
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-96529914-20201102-180710
+ * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-629bbb97-20201207-171303
  */
 
 package com.ibm.cloud.platform_services.enterprise_usage_reports.v1;

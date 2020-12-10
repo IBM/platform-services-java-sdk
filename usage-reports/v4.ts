29a31034ec33da755abbeebb41678b5b3422ba40
Phil Adams <phil_adams@us.ibm.com>
2020-12-10 12:24:29 -0600

    fix(Usage Reports): re-gen service with latest API

--- usage-reports/v4.ts
+++ usage-reports/v4.ts
@@ -15,9 +15,9 @@
  */
 
 /**
- * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-0c6c6df1-20201029-083855
+ * IBM OpenAPI SDK Code Generator Version: 99-SNAPSHOT-d753183b-20201209-163011
  */
- 
+
 
 import * as extend from 'extend';
 import { IncomingHttpHeaders, OutgoingHttpHeaders } from 'http';
@@ -69,7 +69,7 @@ class UsageReportsV4 extends BaseService {
    * Construct a UsageReportsV4 object.
    *
    * @param {Object} options - Options for the service.
-   * @param {string} [options.serviceUrl] - The base url to use when contacting the service (e.g. 'https://gateway.watsonplatform.net'). The base url may differ between IBM Cloud regions.
+   * @param {string} [options.serviceUrl] - The base url to use when contacting the service. The base url may differ between IBM Cloud regions.
    * @param {OutgoingHttpHeaders} [options.headers] - Default headers that shall be included with every request to the service.
    * @param {Authenticator} options.authenticator - The Authenticator object used to authenticate requests to the service
    * @constructor

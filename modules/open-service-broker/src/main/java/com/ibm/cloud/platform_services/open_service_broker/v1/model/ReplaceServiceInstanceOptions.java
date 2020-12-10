02b5fb19d2da48861ad6c7a331cdda796e6a8139
Phil Adams <phil_adams@us.ibm.com>
2020-12-10 10:11:07 -0600

    fix(Open Service Broker): re-gen service with latest API

--- modules/open-service-broker/src/main/java/com/ibm/cloud/platform_services/open_service_broker/v1/model/ReplaceServiceInstanceOptions.java
+++ modules/open-service-broker/src/main/java/com/ibm/cloud/platform_services/open_service_broker/v1/model/ReplaceServiceInstanceOptions.java
@@ -215,9 +215,9 @@ public class ReplaceServiceInstanceOptions extends GenericModel {
   /**
    * Gets the organizationGuid.
    *
-   * Deprecated in favor of `context`. The identifier for the project space within the IBM Cloud platform organization.
-   * Although most brokers will not use this field, it might be helpful for executing operations on a user's behalf. It
-   * MUST be a non-empty string.
+   * Deprecated in favor of `context`. The IBM Cloud platform GUID for the organization under which the service instance
+   * is to be provisioned. Although most brokers will not use this field, it might be helpful for executing operations
+   * on a user's behalf. It MUST be a non-empty string.
    *
    * @return the organizationGuid
    */
@@ -266,9 +266,9 @@ public class ReplaceServiceInstanceOptions extends GenericModel {
   /**
    * Gets the spaceGuid.
    *
-   * Deprecated in favor of `context`. The IBM Cloud platform GUID for the organization under which the service instance
-   * is to be provisioned. Although most brokers will not use this field, it might be helpful for executing operations
-   * on a user's behalf. It MUST be a non-empty string.
+   * Deprecated in favor of `context`. The identifier for the project space within the IBM Cloud platform organization.
+   * Although most brokers will not use this field, it might be helpful for executing operations on a user's behalf. It
+   * MUST be a non-empty string.
    *
    * @return the spaceGuid
    */

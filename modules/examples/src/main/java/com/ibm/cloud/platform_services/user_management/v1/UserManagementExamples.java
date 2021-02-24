ec64e8f9fc1d88652c5daec2daccc8ffeed6182c
Norbert Biczo <pyrooka@users.noreply.github.com>
2021-02-24 21:27:28 +0100

    chore: modify examples

--- modules/examples/src/main/java/com/ibm/cloud/platform_services/user_management/v1/UserManagementExamples.java
+++ modules/examples/src/main/java/com/ibm/cloud/platform_services/user_management/v1/UserManagementExamples.java
@@ -13,12 +13,17 @@
 
 package com.ibm.cloud.platform_services.user_management.v1;
 
+import com.ibm.cloud.platform_services.user_management.v1.model.Attribute;
 import com.ibm.cloud.platform_services.user_management.v1.model.GetUserProfileOptions;
 import com.ibm.cloud.platform_services.user_management.v1.model.GetUserSettingsOptions;
+import com.ibm.cloud.platform_services.user_management.v1.model.InviteUser;
+import com.ibm.cloud.platform_services.user_management.v1.model.InviteUserIamPolicy;
 import com.ibm.cloud.platform_services.user_management.v1.model.InviteUsersOptions;
 import com.ibm.cloud.platform_services.user_management.v1.model.InvitedUserList;
 import com.ibm.cloud.platform_services.user_management.v1.model.ListUsersOptions;
 import com.ibm.cloud.platform_services.user_management.v1.model.RemoveUserOptions;
+import com.ibm.cloud.platform_services.user_management.v1.model.Resource;
+import com.ibm.cloud.platform_services.user_management.v1.model.Role;
 import com.ibm.cloud.platform_services.user_management.v1.model.UpdateUserProfileOptions;
 import com.ibm.cloud.platform_services.user_management.v1.model.UpdateUserSettingsOptions;
 import com.ibm.cloud.platform_services.user_management.v1.model.UserList;
@@ -31,21 +36,118 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//
+// This file provides an example of how to use the User Management service.
+//
+// The following configuration properties are assumed to be defined:
+//
+// USER_MANAGEMENT_URL=<service url>
+// USER_MANAGEMENT_AUTHTYPE=iam
+// USER_MANAGEMENT_AUTH_URL=<IAM token service URL - omit this if using the production environment>
+// USER_MANAGEMENT_APIKEY=<IAM apikey>
+// USER_MANAGEMENT_ACCOUNT_ID=<account ID>
+// USER_MANAGEMENT_USER_ID=<user ID>
+// USER_MANAGEMENT_MEMBER_EMAIL=<member email to invite>
+// USER_MANAGEMENT_VIEWER_ROLE_ID=<viewer role ID>
+// USER_MANAGEMENT_ACCESS_GROUP_ID=<access group ID>
+// # alternateService
+// USER_MANAGEMENT_ADMIN_URL=<service url>
+// USER_MANAGEMENT_ADMIN_AUTHTYPE=iam
+// USER_MANAGEMENT_ADMIN_AUTH_URL=<IAM token service URL - omit this if using the production environment>
+// USER_MANAGEMENT_ADMIN_APIKEY=<IAM apikey>
+//
+// These configuration properties can be exported as environment variables, or stored
+// in a configuration file and then:
+// export IBM_CREDENTIALS_FILE=<name of configuration file>
+//
 public class UserManagementExamples {
   private static final Logger logger = LoggerFactory.getLogger(UserManagementExamples.class);
   protected UserManagementExamples() { }
 
+  private static String accountId;
+  private static String userId;
+  private static String memberEmail;
+  private static String viewerRoleId;
+  private static String accessGroupId;
+
+  private static String deleteUserId;
+
+  static {
+    System.setProperty("IBM_CREDENTIALS_FILE", "../../user_management.env");
+  }
+
   @SuppressWarnings("checkstyle:methodlength")
   public static void main(String[] args) throws Exception {
-    UserManagement service = UserManagement.newInstance();
+    UserManagement service = UserManagement.newInstance(UserManagement.DEFAULT_SERVICE_NAME);
+    UserManagement adminService = UserManagement.newInstance("USER_MANAGEMENT_ADMIN");
 
     // Load up our test-specific config properties.
     Map<String, String> config = CredentialUtils.getServiceProperties(UserManagement.DEFAULT_SERVICE_NAME);
 
+    accountId = config.get("ACCOUNT_ID");
+    userId = config.get("USER_ID");
+    memberEmail = config.get("MEMBER_EMAIL");
+    viewerRoleId = config.get("VIEWER_ROLE_ID");
+    accessGroupId = config.get("ACCESS_GROUP_ID");
+
+    try {
+      // begin-invite_users
+      InviteUser inviteUserModel = new InviteUser.Builder()
+              .email(memberEmail)
+              .accountRole("Member")
+              .build();
+
+      Role roleModel = new Role.Builder()
+              .roleId(viewerRoleId)
+              .build();
+
+      Attribute attributeModel = new Attribute.Builder()
+              .name("accountId")
+              .value(accountId)
+              .build();
+
+      Attribute attributeModel2 = new Attribute.Builder()
+              .name("resourceGroupId")
+              .value("*")
+              .build();
+
+      Attribute[] attributeArray = new Attribute[] { attributeModel, attributeModel2 };
+
+      Resource resourceModel = new Resource.Builder()
+              .attributes(new java.util.ArrayList<Attribute>(java.util.Arrays.asList(attributeArray)))
+              .build();
+
+      InviteUserIamPolicy inviteUserIamPolicyModel = new InviteUserIamPolicy.Builder()
+              .type("access")
+              .roles(new java.util.ArrayList<Role>(java.util.Arrays.asList(roleModel)))
+              .resources(new java.util.ArrayList<Resource>(java.util.Arrays.asList(resourceModel)))
+              .build();
+
+      InviteUsersOptions inviteUsersOptions = new InviteUsersOptions.Builder()
+              .accountId(accountId)
+              .users(new java.util.ArrayList<InviteUser>(java.util.Arrays.asList(inviteUserModel)))
+              .iamPolicy(new java.util.ArrayList<InviteUserIamPolicy>(
+                      java.util.Arrays.asList(inviteUserIamPolicyModel)))
+              .accessGroups(new java.util.ArrayList<String>(java.util.Arrays.asList(accessGroupId)))
+              .build();
+
+      Response<InvitedUserList> response = adminService.inviteUsers(inviteUsersOptions).execute();
+      InvitedUserList invitedUserList = response.getResult();
+
+      System.out.println(invitedUserList);
+      // end-invite_users
+      deleteUserId = invitedUserList.getResources().get(0).getId();
+    } catch (ServiceResponseException e) {
+        logger.error(String.format("Service returned status code %s: %s\nError details: %s",
+          e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()), e);
+    }
+
     try {
       // begin-list_users
       ListUsersOptions listUsersOptions = new ListUsersOptions.Builder()
-        .accountId("testString")
+        .accountId(accountId)
+        .state("ACTIVE")
+        .limit(100)
         .build();
 
       Response<UserList> response = service.listUsers(listUsersOptions).execute();
@@ -59,16 +161,14 @@ public class UserManagementExamples {
     }
 
     try {
-      // begin-invite_users
-      InviteUsersOptions inviteUsersOptions = new InviteUsersOptions.Builder()
-        .accountId("testString")
+      // begin-remove_user
+      RemoveUserOptions removeUserOptions = new RemoveUserOptions.Builder()
+        .accountId(accountId)
+        .iamId(deleteUserId)
         .build();
 
-      Response<InvitedUserList> response = service.inviteUsers(inviteUsersOptions).execute();
-      InvitedUserList invitedUserList = response.getResult();
-
-      System.out.println(invitedUserList);
-      // end-invite_users
+      service.removeUser(removeUserOptions).execute();
+      // end-remove_user
     } catch (ServiceResponseException e) {
         logger.error(String.format("Service returned status code %s: %s\nError details: %s",
           e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()), e);
@@ -77,8 +177,8 @@ public class UserManagementExamples {
     try {
       // begin-get_user_profile
       GetUserProfileOptions getUserProfileOptions = new GetUserProfileOptions.Builder()
-        .accountId("testString")
-        .iamId("testString")
+        .accountId(accountId)
+        .iamId(userId)
         .build();
 
       Response<UserProfile> response = service.getUserProfile(getUserProfileOptions).execute();
@@ -94,8 +194,9 @@ public class UserManagementExamples {
     try {
       // begin-update_user_profile
       UpdateUserProfileOptions updateUserProfileOptions = new UpdateUserProfileOptions.Builder()
-        .accountId("testString")
-        .iamId("testString")
+        .accountId(accountId)
+        .iamId(userId)
+        .phonenumber("123456789")
         .build();
 
       service.updateUserProfile(updateUserProfileOptions).execute();
@@ -108,8 +209,8 @@ public class UserManagementExamples {
     try {
       // begin-get_user_settings
       GetUserSettingsOptions getUserSettingsOptions = new GetUserSettingsOptions.Builder()
-        .accountId("testString")
-        .iamId("testString")
+        .accountId(accountId)
+        .iamId(userId)
         .build();
 
       Response<UserSettings> response = service.getUserSettings(getUserSettingsOptions).execute();
@@ -125,26 +226,14 @@ public class UserManagementExamples {
     try {
       // begin-update_user_settings
       UpdateUserSettingsOptions updateUserSettingsOptions = new UpdateUserSettingsOptions.Builder()
-        .accountId("testString")
-        .iamId("testString")
+        .accountId(accountId)
+        .iamId(userId)
+        .selfManage(true)
+        .allowedIpAddresses("192.168.0.2,192.168.0.3")
         .build();
 
       service.updateUserSettings(updateUserSettingsOptions).execute();
       // end-update_user_settings
-    } catch (ServiceResponseException e) {
-        logger.error(String.format("Service returned status code %s: %s\nError details: %s",
-          e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()), e);
-    }
-
-    try {
-      // begin-remove_user
-      RemoveUserOptions removeUserOptions = new RemoveUserOptions.Builder()
-        .accountId("testString")
-        .iamId("testString")
-        .build();
-
-      service.removeUser(removeUserOptions).execute();
-      // end-remove_user
     } catch (ServiceResponseException e) {
         logger.error(String.format("Service returned status code %s: %s\nError details: %s",
           e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()), e);

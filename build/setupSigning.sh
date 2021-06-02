0aca12434510670b5a83cfb7a607ec3304ddf1a2
Phil Adams <padamstx@gmail.com>
2021-06-02 13:59:22 -0500

    fix(build): correct deployment errors (#105)

--- build/setupSigning.sh
+++ build/setupSigning.sh
@@ -2,7 +2,7 @@
 set -x
 
 echo "Importing signing key..."
-openssl aes-256-cbc -K $encrypted_4c2ca4326cdc_key -iv $encrypted_4c2ca4326cdc_iv -in build/signing.key.enc -out build/signing.key -d
+openssl aes-256-cbc -K $encrypted_4b7d603e7466_key -iv $encrypted_4b7d603e7466_iv -in build/signing.key.enc -out build/signing.key -d
 gpg --version
 gpg --fast-import build/signing.key
 rm build/signing.key

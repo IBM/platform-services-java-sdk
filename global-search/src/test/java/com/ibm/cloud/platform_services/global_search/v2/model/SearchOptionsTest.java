16a358f4ac7b5eeae559e5305dd84bd57e835d44
Fabrizio Leoni <fabrizio_leoni@it.ibm.com>
2023-01-12 15:04:16 +0100

    Upd_1201

---
+++ global-search/src/test/java/com/ibm/cloud/platform_services/global_search/v2/model/SearchOptionsTest.java
@@ -0,0 +1,67 @@
+/*
+ * (C) Copyright IBM Corp. 2023.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.ibm.cloud.platform_services.global_search.v2.model;
+
+import com.ibm.cloud.platform_services.global_search.v2.model.SearchOptions;
+import com.ibm.cloud.platform_services.global_search.v2.utils.TestUtilities;
+import com.ibm.cloud.sdk.core.service.model.FileWithMetadata;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+/**
+ * Unit test class for the SearchOptions model.
+ */
+public class SearchOptionsTest {
+  final HashMap<String, InputStream> mockStreamMap = TestUtilities.createMockStreamMap();
+  final List<FileWithMetadata> mockListFileWithMetadata = TestUtilities.creatMockListFileWithMetadata();
+
+  @Test
+  public void testSearchOptions() throws Throwable {
+    SearchOptions searchOptionsModel = new SearchOptions.Builder()
+      .query("testString")
+      .fields(java.util.Arrays.asList("testString"))
+      .searchCursor("testString")
+      .transactionId("testString")
+      .accountId("testString")
+      .boundary("global")
+      .limit(Long.valueOf("1"))
+      .timeout(Long.valueOf("0"))
+      .sort(java.util.Arrays.asList("testString"))
+      .isDeleted("false")
+      .isReclaimed("false")
+      .isPublic("false")
+      .impersonateUser("testString")
+      .canTag("false")
+      .isHidden("false")
+      .build();
+    assertEquals(searchOptionsModel.query(), "testString");
+    assertEquals(searchOptionsModel.fields(), java.util.Arrays.asList("testString"));
+    assertEquals(searchOptionsModel.searchCursor(), "testString");
+    assertEquals(searchOptionsModel.transactionId(), "testString");
+    assertEquals(searchOptionsModel.accountId(), "testString");
+    assertEquals(searchOptionsModel.boundary(), "global");
+    assertEquals(searchOptionsModel.limit(), Long.valueOf("1"));
+    assertEquals(searchOptionsModel.timeout(), Long.valueOf("0"));
+    assertEquals(searchOptionsModel.sort(), java.util.Arrays.asList("testString"));
+    assertEquals(searchOptionsModel.isDeleted(), "false");
+    assertEquals(searchOptionsModel.isReclaimed(), "false");
+    assertEquals(searchOptionsModel.isPublic(), "false");
+    assertEquals(searchOptionsModel.impersonateUser(), "testString");
+    assertEquals(searchOptionsModel.canTag(), "false");
+    assertEquals(searchOptionsModel.isHidden(), "false");
+  }
+}
---
+++ global-tagging/src/test/java/com/ibm/cloud/platform_services/global_tagging/v1/model/DetachTagOptionsTest.java
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright IBM Corp. 2023.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.ibm.cloud.platform_services.global_tagging.v1.model;
+
+import com.ibm.cloud.platform_services.global_tagging.v1.model.DetachTagOptions;
+import com.ibm.cloud.platform_services.global_tagging.v1.model.Resource;
+import com.ibm.cloud.platform_services.global_tagging.v1.utils.TestUtilities;
+import com.ibm.cloud.sdk.core.service.model.FileWithMetadata;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+/**
+ * Unit test class for the DetachTagOptions model.
+ */
+public class DetachTagOptionsTest {
+  final HashMap<String, InputStream> mockStreamMap = TestUtilities.createMockStreamMap();
+  final List<FileWithMetadata> mockListFileWithMetadata = TestUtilities.creatMockListFileWithMetadata();
+
+  @Test
+  public void testDetachTagOptions() throws Throwable {
+    Resource resourceModel = new Resource.Builder()
+      .resourceId("testString")
+      .resourceType("testString")
+      .build();
+    assertEquals(resourceModel.resourceId(), "testString");
+    assertEquals(resourceModel.resourceType(), "testString");
+
+    DetachTagOptions detachTagOptionsModel = new DetachTagOptions.Builder()
+      .resources(java.util.Arrays.asList(resourceModel))
+      .tagName("testString")
+      .tagNames(java.util.Arrays.asList("testString"))
+      .transactionId("testString")
+      .impersonateUser("testString")
+      .accountId("testString")
+      .tagType("user")
+      .build();
+    assertEquals(detachTagOptionsModel.resources(), java.util.Arrays.asList(resourceModel));
+    assertEquals(detachTagOptionsModel.tagName(), "testString");
+    assertEquals(detachTagOptionsModel.tagNames(), java.util.Arrays.asList("testString"));
+    assertEquals(detachTagOptionsModel.transactionId(), "testString");
+    assertEquals(detachTagOptionsModel.impersonateUser(), "testString");
+    assertEquals(detachTagOptionsModel.accountId(), "testString");
+    assertEquals(detachTagOptionsModel.tagType(), "user");
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testDetachTagOptionsError() throws Throwable {
+    new DetachTagOptions.Builder().build();
+  }
+
+}

deb1a44402d9260f232d5459975814372e39efc1
Phil Adams <phil_adams@us.ibm.com>
2023-01-16 14:17:33 -0600

    fix(Global Search): regen service with latest API definition
    Signed-off-by: Phil Adams <phil_adams@us.ibm.com>

--- modules/global-search/src/main/java/com/ibm/cloud/platform_services/global_search/v2/model/SupportedTypesList.java
+++ modules/global-search/src/main/java/com/ibm/cloud/platform_services/global_search/v2/model/SupportedTypesList.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2021.
+ * (C) Copyright IBM Corp. 2023.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -18,17 +18,19 @@ import com.google.gson.annotations.SerializedName;
 import com.ibm.cloud.sdk.core.service.model.GenericModel;
 
 /**
- * A list of all GhoST indices.
+ * A list of all GhoST indexes.
  */
 public class SupportedTypesList extends GenericModel {
 
   @SerializedName("supported_types")
   protected List<String> supportedTypes;
 
+  protected SupportedTypesList() { }
+
   /**
    * Gets the supportedTypes.
    *
-   * A list of all GhoST indices.
+   * A list of all GhoST indexes.
    *
    * @return the supportedTypes
    */

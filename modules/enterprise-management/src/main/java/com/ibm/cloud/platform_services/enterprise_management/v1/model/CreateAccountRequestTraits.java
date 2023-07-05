508f17e085027ad6b8dd4eb9a0e7b634860989d5
namratabhadauriya <namrata.nitw@gmail.com>
2023-07-05 23:05:40 +0530

    feat(Enterprise Management): re-gen service after update trait object (#199)
    Signed-off-by: namratabhadauriya <namrata.nitw@gmail.com>

--- modules/enterprise-management/src/main/java/com/ibm/cloud/platform_services/enterprise_management/v1/model/CreateAccountRequestTraits.java
+++ modules/enterprise-management/src/main/java/com/ibm/cloud/platform_services/enterprise_management/v1/model/CreateAccountRequestTraits.java
@@ -12,21 +12,26 @@
  */
 package com.ibm.cloud.platform_services.enterprise_management.v1.model;
 
+import com.google.gson.annotations.SerializedName;
 import com.ibm.cloud.sdk.core.service.model.GenericModel;
 
 /**
- * The traits object can be used to opt-out of Multi-Factor Authentication setting when creating a child account in the
+ * The traits object can be used to set properties on child accounts of an enterprise. You can pass a field to opt-out
+ * of Multi-Factor Authentication setting or setup enterprise IAM settings when creating a child account in the
  * enterprise. This is an optional field.
  */
 public class CreateAccountRequestTraits extends GenericModel {
 
   protected String mfa;
+  @SerializedName("enterprise_iam_managed")
+  protected Boolean enterpriseIamManaged;
 
   /**
    * Builder.
    */
   public static class Builder {
     private String mfa;
+    private Boolean enterpriseIamManaged;
 
     /**
      * Instantiates a new Builder from an existing CreateAccountRequestTraits instance.
@@ -35,6 +40,7 @@ public class CreateAccountRequestTraits extends GenericModel {
      */
     private Builder(CreateAccountRequestTraits createAccountRequestTraits) {
       this.mfa = createAccountRequestTraits.mfa;
+      this.enterpriseIamManaged = createAccountRequestTraits.enterpriseIamManaged;
     }
 
     /**
@@ -62,12 +68,24 @@ public class CreateAccountRequestTraits extends GenericModel {
       this.mfa = mfa;
       return this;
     }
+
+    /**
+     * Set the enterpriseIamManaged.
+     *
+     * @param enterpriseIamManaged the enterpriseIamManaged
+     * @return the CreateAccountRequestTraits builder
+     */
+    public Builder enterpriseIamManaged(Boolean enterpriseIamManaged) {
+      this.enterpriseIamManaged = enterpriseIamManaged;
+      return this;
+    }
   }
 
   protected CreateAccountRequestTraits() { }
 
   protected CreateAccountRequestTraits(Builder builder) {
     mfa = builder.mfa;
+    enterpriseIamManaged = builder.enterpriseIamManaged;
   }
 
   /**
@@ -82,13 +100,25 @@ public class CreateAccountRequestTraits extends GenericModel {
   /**
    * Gets the mfa.
    *
-   * By default MFA will be set on the account. To opt out, pass the traits object with the mfa field set to empty
-   * string.
+   * By default MFA will be enabled on a child account. To opt out, pass the traits object with the mfa field set to
+   * empty string. This is an optional field.
    *
    * @return the mfa
    */
   public String mfa() {
     return mfa;
+  }
+
+  /**
+   * Gets the enterpriseIamManaged.
+   *
+   * The Enterprise IAM settings property will be turned off for a newly created child account by default. You can
+   * enable this property by passing 'true' in this boolean field. This is an optional field.
+   *
+   * @return the enterpriseIamManaged
+   */
+  public Boolean enterpriseIamManaged() {
+    return enterpriseIamManaged;
   }
 }
 

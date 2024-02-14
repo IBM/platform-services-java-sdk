8e9124a9b84e79ab8b61adaa58c12f73c3522097
manu.k.m <manu.k.m@ibm.com>
2024-02-14 13:13:48 +0530

    feat(Partner Usage Reports): Update code with latest version
    Signed-off-by: manu.k.m <manu.k.m@ibm.com>

--- modules/partner-usage-reports/src/main/java/com/ibm/cloud/platform_services/partner_usage_reports/v1/PartnerUsageReports.java
+++ modules/partner-usage-reports/src/main/java/com/ibm/cloud/platform_services/partner_usage_reports/v1/PartnerUsageReports.java
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 3.75.0-726bc7e3-20230713-221716
+ * IBM OpenAPI SDK Code Generator Version: 3.85.0-75c38f8f-20240206-210220
  */
 
 package com.ibm.cloud.platform_services.partner_usage_reports.v1;

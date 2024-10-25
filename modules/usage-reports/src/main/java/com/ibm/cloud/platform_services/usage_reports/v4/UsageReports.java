2188d3b4971d514f16f862d461cb65ecabf3b939
Jonathan Date-chong <jtdatechong@gmail.com>
2024-10-25 10:59:42 -0700

    add usage reports generator version update
    Signed-off-by: Jonathan Date-chong <jtdatechong@gmail.com>

--- modules/usage-reports/src/main/java/com/ibm/cloud/platform_services/usage_reports/v4/UsageReports.java
+++ modules/usage-reports/src/main/java/com/ibm/cloud/platform_services/usage_reports/v4/UsageReports.java
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 3.75.0-726bc7e3-20230713-221716
+ * IBM OpenAPI SDK Code Generator Version: 3.96.0-d6dec9d7-20241008-212902
  */
 
 package com.ibm.cloud.platform_services.usage_reports.v4;

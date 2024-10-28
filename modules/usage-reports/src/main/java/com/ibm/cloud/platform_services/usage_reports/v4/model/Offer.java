fd0eee92cd731fe1c31a9ab62a48ff31ba631e4f
Jdatechong <jtdatechong@gmail.com>
2024-10-28 03:01:45 -0700

    feat(Usage Reports): add "created_by_email_id" to Offer schema (#257)
    Signed-off-by: Jonathan Date-chong <jtdatechong@ibm.com>

--- modules/usage-reports/src/main/java/com/ibm/cloud/platform_services/usage_reports/v4/model/Offer.java
+++ modules/usage-reports/src/main/java/com/ibm/cloud/platform_services/usage_reports/v4/model/Offer.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2023.
+ * (C) Copyright IBM Corp. 2024.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -10,6 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
 package com.ibm.cloud.platform_services.usage_reports.v4.model;
 
 import java.util.Date;
@@ -30,6 +31,8 @@ public class Offer extends GenericModel {
   protected String offerTemplate;
   @SerializedName("valid_from")
   protected Date validFrom;
+  @SerializedName("created_by_email_id")
+  protected String createdByEmailId;
   @SerializedName("expires_on")
   protected Date expiresOn;
   protected OfferCredits credits;
@@ -78,6 +81,17 @@ public class Offer extends GenericModel {
    */
   public Date getValidFrom() {
     return validFrom;
+  }
+
+  /**
+   * Gets the createdByEmailId.
+   *
+   * The offer's creator's email id.
+   *
+   * @return the createdByEmailId
+   */
+  public String getCreatedByEmailId() {
+    return createdByEmailId;
   }
 
   /**
--- modules/usage-reports/src/test/java/com/ibm/cloud/platform_services/usage_reports/v4/model/OfferTest.java
+++ modules/usage-reports/src/test/java/com/ibm/cloud/platform_services/usage_reports/v4/model/OfferTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2023.
+ * (C) Copyright IBM Corp. 2024.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -37,6 +37,7 @@ public class OfferTest {
     assertNull(offerModel.getCreditsTotal());
     assertNull(offerModel.getOfferTemplate());
     assertNull(offerModel.getValidFrom());
+    assertNull(offerModel.getCreatedByEmailId());
     assertNull(offerModel.getExpiresOn());
     assertNull(offerModel.getCredits());
   }

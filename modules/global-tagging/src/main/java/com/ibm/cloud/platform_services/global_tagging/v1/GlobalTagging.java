456227b598835db7a6352177db98050f478bfc54
Fabrizio Leoni <fabrizio_leoni@it.ibm.com>
2026-03-24 20:20:58 +0100

    fix(Tagging): Changed query_string pattern in QueryString section
    Signed-off-by: Fabrizio Leoni <fabrizio_leoni@it.ibm.com>

--- modules/global-tagging/src/main/java/com/ibm/cloud/platform_services/global_tagging/v1/GlobalTagging.java
+++ modules/global-tagging/src/main/java/com/ibm/cloud/platform_services/global_tagging/v1/GlobalTagging.java
@@ -12,7 +12,7 @@
  */
 
 /*
- * IBM OpenAPI SDK Code Generator Version: 3.111.0-1bfb72c2-20260206-185521
+ * IBM OpenAPI SDK Code Generator Version: 3.113.1-d76630af-20260320-135953
  */
 
 package com.ibm.cloud.platform_services.global_tagging.v1;
